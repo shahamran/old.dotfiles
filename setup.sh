557cf0047f347443bea9e99b78a8eb4ae129c5ba
Ran Shaham <shahamran91@gmail.com>
2017-10-22 12:24:13 +0300

    some small progress on the setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-programs=( bash compton conky git i3 termite vim X zsh )
-for prog in "${programs[@]}"
+function doOperation {
+  echo -n "$1? [y/N]"
+  read answer
+  if (( answer == "y" )) || (( answer == "Y" )); then
+    "$2"
+  fi
+}
+
+# check if a package manager variable is defined
+if [ -z $PACKMAN ]; then
+  # if not, use Ubuntu's
+  PACKMAN=apt-get
+  UPDATE_COMMAND=update
+  INSTALL_COMMAND=install
+fi
+
+doOperation "update package database" "sudo $PACKMAN $UPDATE_COMMAND"
+
+echo "installing programs..."
+if ( -z $PROGRAMS ); then
+  PROGRAMS=( bash compton conky git i3 termite vim X zsh )
+fi
+for prog in "${PROGRAMS[@]}"
 do
-	echo $prog
+  doOperation "install $prog" "sudo $PACKMAN $INSTALL_COMMAND $prog"
 done;
 
