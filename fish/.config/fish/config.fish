b79422d5c17a78e3574f290572d2fd57da14cca3
Ran Shaham <shahamran91@gmail.com>
2017-10-22 22:51:13 +0300

    remove the dircolors part from fish config and make the fortune short in fish greeting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,3 @@
 set config_home "$HOME/.config"
 
-eval (dircolors -c "$config_home/mintty/dircolors-solarized.256dark")
 source "$config_home/fish/aliases.fish"
--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,3 +1,3 @@
 function fish_greeting
-  fortune | lolcat
+  fortune -s | lolcat
 end
