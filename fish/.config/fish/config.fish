38e9f45e8b5274af041827bee7240a243af9af85
Ran Shaham <shahamran91@gmail.com>
2017-11-07 00:07:06 +0200

    return the mintty dircolors hack to the fish autostart config file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,11 @@ set config_home "$HOME/.config"
 
 source "$config_home/fish/aliases.fish"
 
+# readable ls colors for mintty
+set dircolors_file "$config_home/mintty/dircolors-solarized.256dark"
+if test -e $dircolors_file
+  eval (dircolors -c $dircolors_file) > /dev/null
+end
+set -e dircolors_file
+
+set -e config_home
--- mintty/.config/fish/functions/dircolors.fish
+++
@@ -1,5 +0,0 @@
-# readable ls colors for mintty
-set dircolors_file "$config_home/mintty/dircolors-solarized.256dark"
-if test -e $dircolors_file
-  eval (dircolors -c $dircolors_file)
-end
